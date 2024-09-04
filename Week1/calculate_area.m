74083bccf66736ec7934250e46a42787210ca3cd
gzipprich <gzipprich@Gregorys-MacBook-Air.local>
2024-09-04 18:36:40 -0400

    week one homework progress

---
+++ Week1/calculate_area.m
@@ -0,0 +1,6 @@
+length=17.82;
+width=47.25;
+perimeter=(2*length)+(2*width);
+area=length*width;
+area
+perimeter
---
+++ Week1/week1_script.m
@@ -0,0 +1,17 @@
+a=3; %
+b=4;
+
+
+jittleyang=a*b;
+
+fuhulutoogan=jittleyang+17;
+
+
+fuhulutoogan
+
+%the clc command clears the command window
+
+%the clear command clears your environment/workspace
+
+%the whos command displays lots of information about all of your current
+%variables saved, lie
