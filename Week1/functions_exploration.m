0781395ddf74f48fd7d299ae85b4bc9724d3fcb4
Gregory Zipprich <gzipprich@Gregorys-MacBook-Air.local>
2024-09-08 14:04:24 -0400

    homework done

---
+++ Week1/functions_exploration.m
@@ -0,0 +1,13 @@
+%fprintf function scample
+fprintf('The value of pi is approximately %.5f\n', pi);
+%the %.f5/n is something called a float, basically this allows me to 
+%display pi up to 5 decimal points.
+
+
+%Plot function example
+x = 0:0.1:10;        % creates the x axis from 0 to 10 in steps of .01 
+y = sin(x);          % set y equal to the sin function of x
+plot(x, y);          % this actually plots y(x) or sin(x)
+
+%disp function example
+disp("jittleyang")    %displaying a random name
