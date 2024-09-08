267ffa7ee7be98ecdba518daaf954aaeb7d6cfb7
Gregory Zipprich <gzipprich@Gregorys-MacBook-Air.local>
2024-09-08 14:02:51 -0400

    homework done

---
+++ Week1/fixed_script.m
@@ -0,0 +1,8 @@
+% Task, calculate area of a circle and display the result
+
+radius = 5;
+area = pi * radius^2;
+
+fprintf('The area of the circle is %.3f\n', area)
+%switched the disp function to the fprintf function and added a .3 decimal
+%float
